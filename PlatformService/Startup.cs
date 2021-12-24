250d28443e914eca14b82a63776ffc1692be71fe
John Kehinde <kehindejohn018@gmail.com>
2021-12-24 12:05:05 +0100

    added commands yaml file

--- PlatformService/Startup.cs
+++ PlatformService/Startup.cs
@@ -35,13 +35,16 @@ namespace PlatformService
             services.AddScoped<IPlatformRepo, PlatformRepo>();
 
             services.AddHttpClient<ICommandDataClient, HttpCommandDataClient>();
-            
+
             services.AddControllers();
             services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
             services.AddSwaggerGen(c =>
             {
                 c.SwaggerDoc("v1", new OpenApiInfo { Title = "PlatformService", Version = "v1" });
             });
+
+
+            Console.WriteLine($"--> Command Service Endpoint {Configuration["CommandService"]}");
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
